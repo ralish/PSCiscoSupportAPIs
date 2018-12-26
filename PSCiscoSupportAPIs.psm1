0035fd90459efe3e62e4322e4ae62e3db5b07b69
Samuel D. Leslie <sdl@nexiom.net>
2018-12-26 12:32:20 +1100

    Add Initialize-CiscoApiRequest function

--- PSCiscoSupportAPIs.psm1
+++ PSCiscoSupportAPIs.psm1
@@ -1,6 +1,43 @@
 # See the help for Set-StrictMode for the full details on what this enables.
 Set-StrictMode -Version 2.0
 
+Function Initialize-CiscoApiRequest {
+    [CmdletBinding()]
+    Param()
+
+    $CallerParams = (Get-PSCallStack)[1].InvocationInfo.BoundParameters
+
+    if (!$CallerParams.ContainsKey('ResponseFormat')) {
+        if (Test-Path -Path 'Variable:\CiscoApiResponseFormat') {
+            if ($CiscoApiResponseFormat -in ('JSON', 'PSObject', 'WebResponse')) {
+                Write-Verbose -Message ('Using API response format from $CiscoApiResponseFormat: {0}' -f $CiscoApiResponseFormat)
+                $CallerParams['ResponseFormat'] = $CiscoApiResponseFormat
+            } else {
+                throw 'CiscoApiResponseFormat setting is invalid: {0}' -f $CiscoApiResponseFormat
+            }
+        } else {
+            Write-Verbose -Message 'Using default API response format: PSObject'
+            $CallerParams['ResponseFormat'] = 'PSObject'
+        }
+    }
+
+    $script:RequestCommand = 'Invoke-RestMethod'
+    $script:RequestCommandBaseParams = @{}
+
+    if ($CallerParams['ResponseFormat'] -ne 'PSObject') {
+        $script:RequestCommand = 'Invoke-WebRequest'
+        $script:RequestCommandBaseParams['UseBasicParsing'] = $true
+    }
+
+    if ($CallerParams.ContainsKey('ClientId') -xor $CallerParams.ContainsKey('ClientSecret')) {
+        throw 'You must provide both the ClientId and ClientSecret parameters or neither.'
+    } elseif ($CallerParams.ContainsKey('ClientId') -and $CallerParams.ContainsKey('ClientSecret')) {
+        $script:ApiToken = Get-CiscoApiAccessToken -ClientId $CallerParams['ClientId'] -ClientSecret $CallerParams['ClientSecret']
+    } else {
+        $script:ApiToken = Get-CiscoApiAccessToken
+    }
+}
+
 # https://apiconsole.cisco.com/files/Token_Access.pdf
 Function Get-CiscoApiAccessToken {
     [CmdletBinding()]
